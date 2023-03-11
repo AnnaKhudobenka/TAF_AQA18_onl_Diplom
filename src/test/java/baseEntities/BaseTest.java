00eeadf190005401215840131218a4525f62d5b0
AnnaKhudobenka <annaavinova@gmail.com>
2023-03-11 15:48:31 +0300

    ci/cd

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -10,8 +10,6 @@ import models.Project;
 import models.User;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
 import steps.DashboardStep;
 import steps.LoginStep;
 import steps.ProjectStep;
